b080a5744d92e3585396fa35fc307eb5b9b70502
Michael Yuan <yuan.michael@gmail.com>
2025-02-28 23:32:23 -0800

    Image parser follows sorted order

--- utils/parse_images.py
+++ utils/parse_images.py
@@ -10,19 +10,24 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('src_path')
 arg_parser.add_argument('dest_path')
 arg_parser.add_argument('--stride', '-s', type=int, default=20)
+arg_parser.add_argument('--width', type=int, default=160)
+arg_parser.add_argument('--height', type=int, default=120)
 args = arg_parser.parse_args()
 
 src_dir_path = args.src_path
 dest_dir_path = args.dest_path
 stride = args.stride
+width = args.width
+height = args.height
 
 # Create destination directory
 os.mkdir(dest_dir_path)
 
-for i, filename in enumerate(os.listdir(src_dir_path)):
-  if i % stride == 0:
+filenames = sorted(os.listdir(src_dir_path))
+for i, filename in enumerate(filenames):
+  if filename.endswith('.jpg') and i % stride == 0:
     src_file_path = os.path.join(src_dir_path, filename)
     dest_file_path = os.path.join(dest_dir_path, filename)
     with Image.open(src_file_path) as im:
-      im_resized = im.resize((160, 120), resample=Image.Resampling.NEAREST)
+      im_resized = im.resize((width, height), resample=Image.Resampling.NEAREST)
       im_resized.save(dest_file_path, im_resized.format)
