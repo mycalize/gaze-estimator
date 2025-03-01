302a5527a12b4d423aecebf3bbd8af285f35bddb
Isabelle Hong <isabellehong@g.ucla.edu>
2025-02-28 23:50:41 -0800

    Refactor with functional API

--- training/cnn.py
+++ training/cnn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
+from torch.nn import F
 
 class CNN(torch.nn.Module):
     # for now: (HW 5) [conv - relu - pool] - [conv - relu] - [aff] - softmax
@@ -9,11 +10,9 @@ class CNN(torch.nn.Module):
         self.m1 = torch.nn.Conv2d(1, 128, 3, padding=1)
         self.m2 = torch.nn.Conv2d(128, 256, 3, padding=1)
         self.m3 = torch.nn.Linear(256, 16)
-        self.relu = torch.nn.ReLU()
-        self.pool = torch.nn.MaxPool2d(2)
 
     def forward(self, x):
-        h1 = self.pool(self.relu(self.m1(x)))
-        h2 = self.relu(self.m2(h1))
+        h1 = F.max_pool2d(F.relu(self.m1(x)))
+        h2 = F.relu(self.m2(h1))
         outputs = self.m3(h2)
         return outputs
