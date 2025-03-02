7cb6b19fba6591df361c43cb3acc8bc6ee20880c
Isabelle Hong <isabellehong@g.ucla.edu>
2025-03-01 17:14:36 -0800

    Update imports

--- training/cnn.py
+++ training/cnn.py
@@ -1,15 +1,13 @@
-import numpy as np
-import torch
-from torch.utils.data import TensorDataset, DataLoader
-from torch.nn import F
+import torch.nn as nn
+import torch.nn.functional as F
 
-class CNN(torch.nn.Module):
+class CNN(nn.Module):
     # for now: (HW 5) [conv - relu - pool] - [conv - relu] - [aff] - softmax
     def __init__(self):
         super(CNN, self).__init__()
-        self.m1 = torch.nn.Conv2d(1, 128, 3, padding=1)
-        self.m2 = torch.nn.Conv2d(128, 256, 3, padding=1)
-        self.m3 = torch.nn.Linear(256, 16)
+        self.m1 = nn.Conv2d(1, 128, 3, padding=1)
+        self.m2 = nn.Conv2d(128, 256, 3, padding=1)
+        self.m3 = nn.Linear(256, 16)
 
     def forward(self, x):
         h1 = F.max_pool2d(F.relu(self.m1(x)))
