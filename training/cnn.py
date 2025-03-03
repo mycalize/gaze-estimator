38955b032e5648da378ea7f9d1789432f496bfcd
Isabelle Hong <isabellehong@g.ucla.edu>
2025-03-02 17:48:42 -0800

    Add batchnorm layers

--- training/cnn.py
+++ training/cnn.py
@@ -2,19 +2,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ConvNet(nn.Module):
-    # for now: (HW 5) [conv - relu - pool] - [conv - relu] - [aff] - softmax
+    # for now: (HW 5) [conv - bn - relu - pool] - [conv - bn - relu] - [aff] - softmax
     def __init__(self, input_dims):
         super(ConvNet, self).__init__()
         self.h, self.w = input_dims
-        self.m1 = nn.Conv2d(1, 8, 3, padding=1)
-        self.m2 = nn.Conv2d(8, 4, 3, padding=1)
-        self.lin_in_features = 4 * (self.h // 2) * (self.w // 2)
-        self.m3 = nn.Linear(self.lin_in_features, 16)
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1)
+        self.conv1_bn = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(8, 4, 3, padding=1)
+        self.conv2_bn = nn.BatchNorm2d(4)
+        self.fc_in_features = 4 * (self.h // 2) * (self.w // 2)
+        self.fc = nn.Linear(self.fc_in_features, 16)
 
     def forward(self, x):
-        z1 = F.relu(self.m1(x))
+        z1 = F.relu(self.conv1_bn(self.conv1(x)))
         h1 = F.max_pool2d(z1, kernel_size=2)
-        h2 = F.relu(self.m2(h1))
-        h2 = h2.view(-1, self.lin_in_features)
-        outputs = self.m3(h2)
+        h2 = F.relu(self.conv2_bn(self.conv2(h1)))
+        h2 = h2.view(-1, self.fc_in_features)
+        outputs = self.fc(h2)
         return outputs
