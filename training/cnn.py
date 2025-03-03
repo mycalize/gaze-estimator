20dd645027153c71b67db2effce9221b4c5db730
Isabelle Hong <isabellehong@g.ucla.edu>
2025-03-02 16:26:37 -0800

    Add input dims as C onvNet param

--- training/cnn.py
+++ training/cnn.py
@@ -3,18 +3,18 @@ import torch.nn.functional as F
 
 class ConvNet(nn.Module):
     # for now: (HW 5) [conv - relu - pool] - [conv - relu] - [aff] - softmax
-    def __init__(self):
+    def __init__(self, input_dims):
         super(ConvNet, self).__init__()
+        self.h, self.w = input_dims
         self.m1 = nn.Conv2d(1, 128, 3, padding=1)
         self.m2 = nn.Conv2d(128, 256, 3, padding=1)
-        self.m3 = nn.Linear(256*80*60, 16)
+        self.lin_in_features = 256 * (self.h // 2) * (self.w // 2)
+        self.m3 = nn.Linear(self.lin_in_features, 16)
 
     def forward(self, x):
         z1 = F.relu(self.m1(x))
         h1 = F.max_pool2d(z1, kernel_size=2)
-        # 80 * 60
         h2 = F.relu(self.m2(h1))
-        # 80 * 60
-        h2 = h2.view(-1, 256*80*60)
+        h2 = h2.view(-1, self.lin_in_features)
         outputs = self.m3(h2)
         return outputs
