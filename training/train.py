ee33dbfd22768483077b9dae8dcc7a8f1fb85ef6
Michael Yuan <yuan.michael@gmail.com>
2025-03-06 15:49:59 -0800

    Update simple_cnn notebook and rename ConvNet to CNN

--- training/train.py
+++ training/train.py
@@ -1,9 +1,9 @@
 from training.eval import *
 
 def train(model, criterion, optimizer, train_loader, val_loader, epochs=10):
-    train_loss_list = []
-    train_acc_list = []
-    val_acc_list = []
+    train_loss_history = []
+    train_acc_history = []
+    val_acc_history = []
     for epoch in range(1, epochs+1):
         model.train()
         for batch_X, batch_y in train_loader:
@@ -12,13 +12,13 @@ def train(model, criterion, optimizer, train_loader, val_loader, epochs=10):
             loss = criterion(outputs, batch_y)
             loss.backward()
             optimizer.step()
-            train_loss_list.append(loss.item())
+            train_loss_history.append(loss.item())
 
         train_acc = evaluate_acc(model, train_loader)
         valid_acc = evaluate_acc(model, val_loader)
-        train_acc_list.append(train_acc)
-        val_acc_list.append(valid_acc)
+        train_acc_history.append(train_acc)
+        val_acc_history.append(valid_acc)
 
         print(f"| epoch {epoch:2d} | train acc {train_acc:.6f} | valid acc {valid_acc:.6f} |")
 
-    return train_loss_list, train_acc_list, val_acc_list
+    return train_loss_history, train_acc_history, val_acc_history
