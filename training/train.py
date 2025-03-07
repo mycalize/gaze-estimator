d4fb577ab9a590fc346a8520fc073399925af5fd
Michael Yuan <yuan.michael@gmail.com>
2025-03-07 00:04:10 -0800

    Add GPU support

--- training/train.py
+++ training/train.py
@@ -1,12 +1,17 @@
 from training.eval import *
 
-def train(model, criterion, optimizer, train_loader, val_loader, epochs=10):
+def train(model, criterion, optimizer, device, train_loader, val_loader, epochs=10):
     train_loss_history = []
     train_acc_history = []
     val_acc_history = []
+
     for epoch in range(1, epochs+1):
         model.train()
         for batch_X, batch_y in train_loader:
+            # Move data to device
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+
             optimizer.zero_grad()
             outputs = model(batch_X)
             loss = criterion(outputs, batch_y)
@@ -14,8 +19,8 @@ def train(model, criterion, optimizer, train_loader, val_loader, epochs=10):
             optimizer.step()
             train_loss_history.append(loss.item())
 
-        train_acc = evaluate_acc(model, train_loader)
-        valid_acc = evaluate_acc(model, val_loader)
+        train_acc = evaluate_acc(model, device, train_loader)
+        valid_acc = evaluate_acc(model, device, val_loader)
         train_acc_history.append(train_acc)
         val_acc_history.append(valid_acc)
 
