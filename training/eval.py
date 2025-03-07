9ee444f6b93ec881bfb9921eef1a1114efd828e9
Michael Yuan <yuan.michael@gmail.com>
2025-03-06 19:16:50 -0800

    Add GPU support

--- training/eval.py
+++ training/eval.py
@@ -1,21 +1,15 @@
 import torch
 
-def evaluate_loss(model, criterion, dataloader):
-    model.eval()
-    total_loss = 0.0
-    for batch_X, batch_y in dataloader:
-        outputs = model(batch_X)
-        loss = criterion(outputs, batch_y)
-        total_loss += loss.item()
-
-    return total_loss / len(dataloader)
-
-def evaluate_acc(model, dataloader):
+def evaluate_acc(model, device, dataloader):
     model.eval()
     total_acc = 0.0
     for batch_X, batch_y in dataloader:
+        # Move data to device
+        batch_X = batch_X.to(device)
+        batch_y = batch_y.to(device)
+
         outputs = model(batch_X)
         predictions = torch.argmax(outputs, dim=1)
         total_acc += (predictions==batch_y).sum()
 
-    return total_acc / len(dataloader.dataset)
+    return total_acc.cpu() / len(dataloader.dataset)
