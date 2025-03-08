7de78e959558fdec89693de97f60e386b44a7784
Michael Yuan <yuan.michael@gmail.com>
2025-03-08 00:45:54 -0800

    Refactor code to support regression and classification

--- training/eval.py
+++ training/eval.py
@@ -3,45 +3,39 @@ import torch
 # from data.convert_labels import *
 
 def evaluate_acc(model, device, dataloader):
-    model.eval()
-    total_acc = 0.0
-    for batch_X, batch_y in dataloader:
-        # Move data to device
-        batch_X = batch_X.to(device)
-        batch_y = batch_y.to(device)
+  model.eval()
+  total_acc = 0.0
+  for batch_X, batch_y in dataloader:
+    # Move data to device
+    batch_X = batch_X.to(device)
+    batch_y = batch_y.to(device)
 
-        outputs = model(batch_X)
+    outputs = model(batch_X)
 
-        # Classify labels after regression
-        # target_transform = lambda target: convert_labels(target)
-        # batch_y = np.array([target_transform(y) for y in batch_y], dtype='float32')
-        # predictions = np.array([target_transform(output) for output in outputs], dtype='float32')
+    predictions = torch.argmax(outputs, dim=1)
+    total_acc += (predictions.detach() == batch_y).sum()
 
-        predictions = torch.argmax(outputs, dim=1)
-        total_acc += (predictions==batch_y).sum()
-
-    return total_acc.cpu() / len(dataloader.dataset)
+  return total_acc.cpu() / len(dataloader.dataset)
 
 def evaluate_error(model, device, dataloader):
-    model.eval()
-    total_angles = 0.0
+  model.eval()
+  total_angles = 0.0
+  for batch_X, batch_y in dataloader:
+    # Move data to device
+    batch_X = batch_X.to(device)
+    batch_y = batch_y.to(device)
 
-    def angle_between(gaze_x1, gaze_y1, gaze_x2, gaze_y2):
-        cos_sim = torch.cos(gaze_y1) * torch.cos(gaze_y2)
-        cos_sim *= torch.cos(gaze_x1 - gaze_x2)
-        cos_sim += torch.sin(gaze_y1) * torch.sin(gaze_y2)
-        return torch.acos(cos_sim)
+    predictions = model(batch_X)
+    gaze_x, gaze_y = batch_y[:, 0], batch_y[:, 1]
+    predictions_detached = predictions.detach()
+    gaze_x_pred, gaze_y_pred = predictions_detached[:, 0], predictions_detached[:, 1]
 
-    for batch_X, batch_y in dataloader:
-        # Move data to device
-        batch_X = batch_X.to(device)
-        batch_y = batch_y.to(device)
+    total_angles += __angle_diff(gaze_x, gaze_y, gaze_x_pred, gaze_y_pred).sum()
 
-        predictions = model(batch_X)
-        gaze_x, gaze_y = batch_y[:, 0], batch_y[:, 1]
-        gaze_x_pred, gaze_y_pred = predictions[:, 0], predictions[:, 1]
+  return total_angles.cpu() / len(dataloader.dataset)
 
-        print(torch.nn.MSELoss()(predictions, batch_y).item())
-        total_angles += angle_between(gaze_x, gaze_y, gaze_x_pred, gaze_y_pred).sum()
-
-    return total_angles.cpu() / len(dataloader.dataset)
+def __angle_diff(gaze_x1, gaze_y1, gaze_x2, gaze_y2):
+  cos_sim = torch.cos(gaze_y1) * torch.cos(gaze_y2)
+  cos_sim *= torch.cos(gaze_x1 - gaze_x2)
+  cos_sim += torch.sin(gaze_y1) * torch.sin(gaze_y2)
+  return torch.acos(cos_sim)
--- training/train.py
+++ training/train.py
@@ -1,30 +1,36 @@
 from training.eval import *
 
-def train(model, criterion, optimizer, device, train_loader, val_loader, epochs=10):
-    train_loss_history = []
-    train_acc_history = []
-    val_acc_history = []
+def train(model, device, mode, criterion, optimizer, train_loader, val_loader, epochs=10):
+  train_loss_history = []
+  train_perf_history = []
+  val_perf_history = []
 
-    for epoch in range(1, epochs+1):
-        model.train()
-        for batch_X, batch_y in train_loader:
-            # Move data to device
-            batch_X = batch_X.to(device)
-            batch_y = batch_y.to(device)
+  for epoch in range(1, epochs+1):
+    model.train()
+    for batch_X, batch_y in train_loader:
+      # Move data to device
+      batch_X = batch_X.to(device)
+      batch_y = batch_y.to(device)
 
-            optimizer.zero_grad()
-            outputs = model(batch_X)
-            loss = criterion(outputs, batch_y)
-            loss.backward()
-            optimizer.step()
-            train_loss_history.append(loss.item())
+      optimizer.zero_grad()
+      outputs = model(batch_X)
+      loss = criterion(outputs, batch_y)
+      loss.backward()
+      optimizer.step()
+      train_loss_history.append(loss.item())
 
-        # train_acc = evaluate_acc(model, device, train_loader)
-        # valid_acc = evaluate_acc(model, device, val_loader)
-        # train_acc_history.append(train_acc)
-        # val_acc_history.append(valid_acc)
+    if mode == 'reg':
+      train_perf = evaluate_error(model, device, train_loader)
+      valid_perf = evaluate_error(model, device, val_loader)
+      print(f"| Epoch {epoch:2d} | Train err {train_perf:.6f} | Valid err {valid_perf:.6f} |")
+    else:
+      train_perf = evaluate_acc(model, device, train_loader)
+      valid_perf = evaluate_acc(model, device, val_loader)
+      print(f"| Epoch {epoch:2d} | Train acc {train_perf:.6f} | Valid err {valid_perf:.6f} |")
 
-        # print(f"| epoch {epoch:2d} | train acc {train_acc:.6f} | valid acc {valid_acc:.6f} |")
+    train_perf_history.append(train_perf)
+    val_perf_history.append(valid_perf)
 
-    return train_loss_history
-    # return train_loss_history, train_acc_history, val_acc_history
+    print(f'Current allocated memory (B): {torch.mps.current_allocated_memory()}')
+
+  return train_loss_history, train_perf_history, val_perf_history
